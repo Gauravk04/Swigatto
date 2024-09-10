8b4208c4080c9ef07bc4423b9a0e574f4829c268
f6fb214a-bf1e-4185-8352-482dc141ab89 <gaurav.kumar@strideone.in>
2024-09-10 11:59:31 +0530

    updates in login signup

--- frontend/src/loginSignup.js
+++ frontend/src/loginSignup.js
@@ -1,54 +1,31 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import validateForm from './validation'; // 
 import './loginSignup.css';
 
 function Loginsignup() {
-  const [state, setState] = useState("Login");
-  let navigate = useNavigate();
+    const [state, setState] = useState("Login");
+    let navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    email: ""
-  });
+    const [formData, setFormData] = useState({
+        name: "",
+        password: "",
+        email: "",
+        number: "" // 
+    });
 
-  const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState({}); 
 
-  const changeHandler = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const changeHandler = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-  const validateForm = () => {
-    let validationErrors = {};
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
-
-    if (formData.email === "") {
-      validationErrors.email = "Email should not be empty";
-    } else if (!emailPattern.test(formData.email)) {
-      validationErrors.email = "Email didn't match";
-    } else {
-      validationErrors.email = "";
-    }
-
-    if (formData.password === "") {
-      validationErrors.password = "Password should not be empty";
-    } else if (!passwordPattern.test(formData.password)) {
-      validationErrors.password = "Password should have at least one digit, one lowercase letter, one uppercase letter, and be a minimum of 8 characters long, without special characters.";
-    } else {
-      validationErrors.password = "";
-    }
-
-    if (state === "Sign Up" && formData.username === "") {
-      validationErrors.username = "Please enter your name.";
-    } else {
-      validationErrors.username = "";
-    }
-
-    setErrors(validationErrors);
-    return Object.keys(validationErrors).every((key) => validationErrors[key] === "");
-  };
-
+    const handleValidation = () => {
+        const isSignup = state === "Sign Up";
+        const validationErrors = validateForm(formData, isSignup);
+        setErrors(validationErrors);
+        return Object.keys(validationErrors).every((key) => validationErrors[key] === "");
+    };
   const login = async () => {
     if (!validateForm()) return;
     console.log("Login Function Executed", formData);
@@ -97,50 +74,61 @@ function Loginsignup() {
 
   return (
     <div className="login-bg">
-      <div className="loginsignup">
-        <div className={state === "Sign Up" ? "loginsignup-container signup1-container" : "loginsignup-container login1-container"}>
-          <h1>{state}</h1>
-          <div className="loginsignup-fields">
-            {state === "Sign Up" && (
-              <input
-                name='username'
-                value={formData.username}
-                onChange={changeHandler}
-                type="text"
-                placeholder="Your Name"
-              />
-            )}
-            {errors.username && <p className="error">{errors.username}</p>}
-            <input
-              name='email'
-              value={formData.email}
-              onChange={changeHandler}
-              type="email"
-              placeholder="Email Address"
-            />
-            {errors.email && <p className="error">{errors.email}</p>}
-            <input
-              name='password'
-              value={formData.password}
-              onChange={changeHandler}
-              type="password"
-              placeholder="Password"
-            />
-            {errors.password && <p className="error">{errors.password}</p>}
-          </div>
-          <button onClick={() => { state === "Login" ? login() : signup() }}>Continue</button>
-          {state === "Sign Up" ?
-            <p className="loginsignup-login">Already have an account? <span onClick={() => { setState("Login") }}>Login here</span></p> :
-            <p className="loginsignup-login">Create an account <span onClick={() => { setState("Sign Up") }}>Click here</span></p>}
-          <div className='Terms'>
-            <p style={{ fontSize: '12px' }}>
-              By {state} you are agreeing to our Terms and Conditions
-            </p>
-          </div>
+        <div className="loginsignup">
+            <div className={state === "Sign Up" ? "loginsignup-container signup1-container" : "loginsignup-container login1-container"}>
+                <h1>{state}</h1>
+                <div className="loginsignup-fields">
+                    {state === "Sign Up" && (
+                        <>
+                            <input
+                                name='Name'
+                                value={formData.name}
+                                onChange={changeHandler}
+                                type="text"
+                                placeholder="Your Name"
+                            />
+                            {errors.name && <p className="error">{errors.name}</p>}
+                            
+                            <input
+                                name='Number'
+                                value={formData.number}
+                                onChange={changeHandler}
+                                type="text"
+                                placeholder="Phone Number"
+                            />
+                            {errors.number && <p className="error">{errors.number}</p>}
+                        </>
+                    )}
+                    <input
+                        name='Email'
+                        value={formData.email}
+                        onChange={changeHandler}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                    {errors.email && <p className="error">{errors.email}</p>}
+                    <input
+                        name='Password'
+                        value={formData.password}
+                        onChange={changeHandler}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    {errors.password && <p className="error">{errors.password}</p>}
+                </div>
+                <button onClick={() => { state === "Login" ? login() : signup() }}>Continue</button>
+                {state === "Sign Up" ?
+                    <p className="loginsignup-login">Already have an account? <span onClick={() => { setState("Login") }}>Login here</span></p> :
+                    <p className="loginsignup-login">Create an account <span onClick={() => { setState("Sign Up") }}>Click here</span></p>}
+                <div className='Terms'>
+                    <p style={{ fontSize: '12px' }}>
+                        By {state} you are agreeing to our Terms and Conditions
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  );
+);
 }
 
 export default Loginsignup;
